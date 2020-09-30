0581f29f89ec50d4c4701d2bc3e82ffac02a4176
千橘雫霞 <qianjunakasumi@gmail.com>
2020-09-30 23:49:20 +0800

    其他：编译脚本

---
+++ scripts/build.ps1
@@ -0,0 +1,23 @@
+cd ../
+
+$root = "github.com/CCServe/class/"
+$mainapp = "cmd/class/main.go"
+$commitid = git rev-parse --short master
+
+"开始检查代码错误..."
+
+go vet cmd/class/main.go
+scripts/golangci-lint.exe run
+
+"请确认，按任意键开始编译"
+[Console]::ReadKey() | Out-Null
+
+"开始编译..."
+
+go build -ldflags "-w -X ${root}configs.CommitId=${commitid}" -o build/main.exe ${mainapp}
+
+$env:GOOS="linux"
+go build -ldflags "-w -X ${root}configs.CommitId=${commitid}" -o build/main ${mainapp}
+
+"编译完成，按任意键退出..."
+[Console]::ReadKey() | Out-Null
