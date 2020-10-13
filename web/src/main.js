c33390ec2cae3cd1c25e02faf8c112bad358b866
千橘雫霞 <qianjunakasumi@gmail.com>
2020-10-13 13:19:50 +0800

    其他：使用新依赖

--- web/src/main.js
+++ web/src/main.js
@@ -1,11 +1,14 @@
 import Vue from 'vue'
 import Antd from 'ant-design-vue'
+import Cookie from 'vue-cookies'
+
 import App from './App.vue'
 import router from './router'
 
 import 'ant-design-vue/dist/antd.min.css'
 
 Vue.use(Antd)
+Vue.use(Cookie)
 
 Vue.config.productionTip = false
 
