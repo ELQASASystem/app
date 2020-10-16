0e55d61910d1184d6162867d4e91eb7c27f74775
千橘雫霞 <qianjunakasumi@gmail.com>
2020-10-16 16:42:41 +0800

    史诗：新建答题

---
+++ web/src/views/Answer/Teacher/New.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="wrapper">
+
+    <a-page-header
+        title="新建答题"
+        sub-title="新建一个答题"
+        @back="() => null"
+    >
+      <template slot="extra">
+        <a-button type="primary">答题列表</a-button>
+      </template>
+    </a-page-header>
+
+    <div class="basic">
+
+
+    </div>
+
+
+    <div class="upload">
+      <a-upload-dragger
+          accept=".docx"
+          action="http://localhost:4040/apis/upload/docx"
+          @change="uploadDocx"
+      >
+        <p class="ant-upload-drag-icon">
+          <a-icon type="inbox"/>
+        </p>
+        <p class="ant-upload-text">
+          点击或拖拽文件到这里上传
+        </p>
+        <p class="ant-upload-hint">
+          仅支持单个（.docx）文件上传
+        </p>
+      </a-upload-dragger>
+    </div>
+
+
+  </div>
+
+
+</template>
+
+
+<script>
+export default {
+  name: "New",
+  methods: {
+    uploadDocx(info) {
+
+      console.log(info)
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
