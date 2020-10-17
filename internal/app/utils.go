0740597c49a798aa00f0eeebf2a5a4704c317a0d
千橘雫霞 <qianjunakasumi@gmail.com>
2020-10-17 20:52:44 +0800

    重构：弃用

--- internal/app/utils.go
+++ internal/app/utils.go
@@ -27,53 +27,6 @@ func isValidAnswer(answer string) (ok bool) {
 
 }
 
-// parseAnswer 解析消息中的答案, 并返回 Answer 结构体
-func parseAnswer(m *qq.Msg, aid uint64) (*Answer, bool) {
-
-	s := chainToString(m.Chain)
-
-	if isValidAnswer(s) && !isAnswered(m.Group.ID, m.User.ID) {
-		return &Answer{s, m.User.ID, aid}, true
-	} else {
-		return nil, false
-	}
-
-}
-
-// isAnswered 检查对应 QQ 号用户是否已经答题过了
-func isAnswered(aid uint64, qid uint64) bool {
-
-	if question, _, ok := GetQuestionByID(aid); ok {
-		for _, user := range question.AnsweredUsers {
-			if user.Sender == qid {
-				return true
-			}
-		}
-	}
-	return false
-
-}
-
-// GetQuestionByID 通过问题 ID 获取问题实体
-func GetQuestionByID(aid uint64) (*Question, int, bool) {
-	for i, question := range questionPool {
-		if question.QuestionID == aid {
-			return &question, i, true
-		}
-	}
-	return nil, 0, false
-}
-
-// getQuestionByGroup 通过群号获取问题实体
-func getQuestionByGroup(qid uint64) (*Question, int, bool) {
-	for index, question := range questionPool {
-		if question.TargetGroup == qid {
-			return &question, index, true
-		}
-	}
-	return nil, 0, false
-}
-
 // HashSHA1 将答题数据散列
 func HashSHA1(data interface{}) string {
 
@@ -82,13 +35,4 @@ func HashSHA1(data interface{}) string {
 	h.Write([]byte(fmt.Sprintf("%v", data)))
 
 	return hex.EncodeToString(h.Sum(nil))
-}
-
-// MakeQuestion 制作一个 Question 结构类
-func MakeQuestion(qid uint64, gid uint64, text string) *Question {
-	return &Question{
-		QuestionID:   qid,
-		QuestionText: text,
-		TargetGroup:  gid,
-	}
 }
