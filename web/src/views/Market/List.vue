92440f02f15e474737245bceea4fcceedc5fb7a7
千橘雫霞 <qianjunakasumi@gmail.com>
2020-11-14 22:16:48 +0800

    史诗：学科分类，优化显示 #1

--- web/src/views/Market/List.vue
+++ web/src/views/Market/List.vue
@@ -14,15 +14,38 @@
     </a-page-header>
 
     <div class="list">
+
+      <a-radio-group
+          v-model="subject"
+          default-value="1"
+          button-style="solid"
+          @change="onSubjectChange"
+      >
+        <a-radio-button value="1">语文</a-radio-button>
+        <a-radio-button value="2">数学</a-radio-button>
+        <a-radio-button value="3">英语</a-radio-button>
+        <a-radio-button value="4">物理</a-radio-button>
+        <a-radio-button value="5">历史</a-radio-button>
+        <a-radio-button value="6">化学</a-radio-button>
+        <a-radio-button value="7">生物</a-radio-button>
+        <a-radio-button value="8">地理</a-radio-button>
+        <a-radio-button value="9">政治</a-radio-button>
+        <a-radio-button value="10">其他</a-radio-button>
+      </a-radio-group>
+
+      <a-divider/>
+
       <a-list item-layout="horizontal" :data-source="questionList" :loading="questionListLoad">
         <a-list-item slot="renderItem" slot-scope="item">
 
-          <div v-for="q in JSON.parse(item.question)" :key="q.id">
-            <span v-if="q.type === 'text'">{{ q.text }}</span>
-            <img
-                v-else :src="'/assets/question/pictures/'+q.path"
-                class="question-img" alt="问题图片"
-            >
+          <div>
+            <div v-for="q in JSON.parse(item.question)" :key="q.id">
+              <template v-if="q.type === 'text'">{{ q.text }}</template>
+              <img
+                  v-else :src="'/assets/question/pictures/'+q.path"
+                  class="question-img" alt="问题图片"
+              >
+            </div>
           </div>
           <a-button class="btn-add">添加</a-button>
 
@@ -40,24 +63,31 @@ export default {
   name: "List",
   data() {
     return {
+      subject: 0,
       questionList: [],
       questionListLoad: true
     }
   },
   methods: {
     fetchMarket() {
-      Axios.get('/apis/market/list').then(res => {
+      Axios.get(`/apis/market/${this.subject}/list`).then(res => {
 
         console.log('成功获取问题市场数据：')
         console.log(res.data)
 
-        this.questionListLoad = false
         this.questionList = res.data
 
       }).catch(err => {
         console.error('获取问题市场失败：' + err)
+        this.$notification.error({message: '这个领域还没有人哦'})
+        this.questionList = []
+      }).finally(() => {
+        this.questionListLoad = false
       })
 
+    },
+    onSubjectChange() {
+      this.fetchMarket()
     }
   },
   mounted() {
@@ -73,6 +103,7 @@ export default {
 
 .question-img {
   max-width: 100%;
+  max-height: 320px;
 }
 
 .btn-add {
