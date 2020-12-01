f84bca19dadcd5b5e0797471f52c1ed266b6a105
千橘雫霞 <qianjunakasumi@gmail.com>
2020-12-01 13:45:22 +0800

    史诗：onlineToken 鉴权 #3

--- internal/apis/http/security.go
+++ internal/apis/http/security.go
@@ -1,8 +1,12 @@
 package http
 
+import (
+	"github.com/kataras/iris/v12/context"
+)
+
 // auth 鉴权
 type auth struct {
-	onlineToken map[string]string // onlineToken 用户在线 Token
+	onlineTokenList map[string]onlineToken // onlineTokenList 用户在线 Token 列表
 }
 
 type (
@@ -13,18 +17,24 @@ type (
 // NewAuth 新建一个鉴权
 func NewAuth() *auth { return &auth{} }
 
-// generateLoginToken 使用 u：用户名(User) 生成 loginToken
-func generateLoginToken(u string) loginToken {
+// generateLoginToken 使用 u：用户名 生成 loginToken
+func (a *auth) generateLoginToken(u string) loginToken {
 	return ""
 }
 
-// generateOnlineToken 使用 u：用户名(User) t：登录永久 Token(loginToken) 生成 onlineToken
-func generateOnlineToken(u string, t loginToken) onlineToken {
+// generateOnlineToken 使用 u：用户名 t：loginToken 生成 onlineToken
+func (a *auth) generateOnlineToken(u string, t loginToken) onlineToken {
 	return ""
 }
 
-// verifyOnlineToken 使用 u：用户名(User) t：用户在线 Token(onlineToken) 鉴权
+// verifyOnlineToken onlineToken 鉴权
 // bool 值返回鉴权结果 T：允许 F：禁止
-func (a *auth) verifyOnlineToken(u string, t onlineToken) bool {
-	return true
+func (a *auth) verifyOnlineToken(c *context.Context) (r bool) {
+
+	if c.GetCookie("onlineToken") == string(a.onlineTokenList[c.GetCookie("user")]) {
+		return true
+	}
+
+	c.StatusCode(401)
+	return
 }
