3553252cdf478d70979cc05401dc75c47f63a735
千橘雫霞 <qianjunakasumi@gmail.com>
2020-12-02 13:44:01 +0800

    史诗：生成 onlineToken（用户在线 Token） #3

--- internal/apis/http/security.go
+++ internal/apis/http/security.go
@@ -3,7 +3,9 @@ package http
 import (
 	"bytes"
 	"crypto/rand"
+	"crypto/sha1"
 	"crypto/sha256"
+	"errors"
 	"fmt"
 	"net/http"
 	"time"
@@ -14,16 +16,18 @@ import (
 	"github.com/rs/zerolog/log"
 )
 
-// auth 鉴权
-type auth struct {
-	app             *app.App
-	onlineTokenList map[string]onlineToken // onlineTokenList 用户在线 Token 列表
-}
-
 type (
+	// auth 鉴权
+	auth struct {
+		app             *app.App               // app App
+		onlineTokenList map[string]onlineToken // onlineTokenList 用户在线 Token 列表
+	}
+
 	loginToken  string // loginToken 登录永久 Token
 	onlineToken string // onlineToken 用户在线 Token
 )
+
+var Banned = errors.New("禁止登录")
 
 // NewAuth 新建一个鉴权
 func NewAuth() *auth { return &auth{app.AC, map[string]onlineToken{}} }
@@ -55,12 +59,53 @@ func (a *auth) generateLoginToken(u string, c *context.Context) {
 		Name: "loginToken", Value: string(t),
 		Expires: time.Now().AddDate(0, 1, 0), Secure: true,
 	})
-	return
+
+	_ = a.generateOnlineToken(u, t, c, false)
 }
 
 // generateOnlineToken 使用 u：用户名 t：loginToken 生成 onlineToken
-func (a *auth) generateOnlineToken(u string, t loginToken) onlineToken {
-	return ""
+func (a *auth) generateOnlineToken(u string, lt loginToken, c *context.Context, check bool) (err error) {
+
+	if check {
+		if !a.checkLoginToken(u, lt) {
+			return Banned
+		}
+	}
+
+	var (
+		original = bytes.NewBuffer(nil)
+		salt     = make([]byte, 8)
+	)
+
+	original.WriteString(string(lt))
+	original.WriteString(u)
+	original.Write(salt)
+
+	token := sha1.Sum(original.Bytes())
+	t := onlineToken(fmt.Sprintf("%x", token))
+
+	a.onlineTokenList[u] = t
+	c.SetCookie(&http.Cookie{
+		Name: "loginToken", Value: string(t),
+		Expires: time.Now().AddDate(0, 1, 0), Secure: true,
+	})
+	return
+}
+
+// checkLoginToken 检查 loginToken
+func (a *auth) checkLoginToken(u string, t loginToken) (right bool) {
+
+	res, err := a.app.DB.Account().ReadAccountsList(u)
+	if err != nil {
+		log.Error().Err(err).Msg("读取用户信息出错")
+		return
+	}
+
+	if string(t) == res.LoginToken {
+		return true
+	}
+
+	return
 }
 
 // verifyOnlineToken onlineToken 鉴权
